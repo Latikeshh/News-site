ba3a7b11fdf5391507c66ecd117beae7cb2f5b33
latikeshh <latikesh29@gmail.com>
2025-08-10 14:39:40 +0530

    setup backend

--- AdminPanel/src/Pages/Dashboard1.js
+++ AdminPanel/src/Pages/Dashboard1.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import './dashboard.css';
 
 const Dashboard1 = () => {
-    const [notes, setNotes] = useState([]);
+    const [news, setNotes] = useState([]);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -12,13 +12,13 @@ const Dashboard1 = () => {
     }, []);
 
     const loadNotes = () => {
-        axios.get('http://localhost:8000/getnotes')
+        axios.get('http://localhost:8000/getNews')
             .then(res => setNotes(res.data.data))
             .catch(err => console.error(err));
     };
 
     const softDeleteNote = (id) => {
-        axios.put(`http://localhost:8000/deletenote/${id}`)
+        axios.put(`http://localhost:8000/deleteNews/${id}`)
             .then(() => {
                 alert('Note deleted');
                 loadNotes();
@@ -38,16 +38,23 @@ const Dashboard1 = () => {
             </header>
 
             <div className="dash-notes-wrapper">
-                {notes.length === 0 ? (
+                {news.length === 0 ? (
                     <p className="dash-empty-message">No notes saved yet.</p>
                 ) : (
-                    notes.map((note) => (
-                        <div className="dash-note-card" key={note._id}>
-                            <h3>{note.name || 'Untitled'}</h3>
-                            <p>{note.message || 'Empty note'}</p>
+                    news.map((News) => (
+                        <div className="dash-note-card" key={News._id}>
+                            <h3>{News.title || 'Untitled'}</h3>
+                            <p>{News.content || 'Empty note'}</p>
+                            <p>{News.author || 'Empty note'}</p>
+                            <p>{News.category || 'Empty note'}</p>
+                            <p>
+                                📅 {News.publishedAt
+                                    ? new Date(News.publishedAt).toLocaleDateString('en-GB') // DD/MM/YYYY
+                                    : ''}
+                            </p>
                             <div className="dash-actions">
-                                <button className="dash-edit-btn" onClick={() => editNote(note._id)}>✏️ Edit</button>
-                                <button className="dash-delete-btn" onClick={() => softDeleteNote(note._id)}>🗑️ Delete</button>
+                                <button className="dash-edit-btn" onClick={() => editNote(News._id)}>✏️ Edit</button>
+                                <button className="dash-delete-btn" onClick={() => softDeleteNote(News._id)}>🗑️ Delete</button>
                             </div>
                         </div>
                     ))
