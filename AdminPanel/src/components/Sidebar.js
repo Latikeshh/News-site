ac6386ba32f4c94feec6900bfd988310971f04a1
latikeshh <latikesh29@gmail.com>
2025-08-18 14:13:25 +0530

    dark theme

--- AdminPanel/src/components/Sidebar.js
+++ AdminPanel/src/components/Sidebar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Sidebar = () => (
   <div className="bg-dark text-white p-3 vh-100 position-fixed" style={{ width: '220px' }}>
-    <h4 className="mb-4">News Admin</h4>
+    <h4 className="mb-4">Talk Bharat Admin</h4>
     <Nav defaultActiveKey="/dashboard" className="flex-column">
       <Nav.Link as={Link} to="/dashboard" className="text-white">Dashboard</Nav.Link>
       <Nav.Link as={Link} to="/categories" className="text-white">Categories</Nav.Link>
